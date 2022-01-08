1196ecabc90969c1e7e32bf47af2fd569ebd1db5
Quang Nguyen <vuquangkhtn@gmail.com>
2022-01-08 17:22:58 +0700

    Add custom module defined type

---
+++ typings/my-porfolio-common-library.d.ts
@@ -0,0 +1,10 @@
+declare module 'my-porfolio-common-library' {
+  const Button;
+  const Input;
+  const TextArea;
+  const Link;
+  const NavBarToggler;
+  const Icon;
+  const confirmKit;
+  const StaticPages;
+}
