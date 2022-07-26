6a5f29b478a7ebc4d8509514f0466a0739648f11
Quang Nguyen <vuquangkhtn@gmail.com>
2022-07-26 09:20:21 +0700

    Update link to common lib

--- api/article.ts
+++ api/article.ts
@@ -1,28 +1,28 @@
-import imgMediumSrc from 'static/images/medium.png';
+import imgMediumSrc from "static/images/medium.png";
 
 const articles = [
-  {
-    id: 'article-1',
-    thumbnail: imgMediumSrc,
-    url: 'https://medium.com/geekculture/how-to-manage-states-in-react-applications-effectively-d31b2f740496',
-    title: 'How to manage states in React applications effectively',
-    description:
-      'you can keep not only a clean and readable state management but also reduce the complex logic handling in your applications.',
-  },
-  {
-    id: 'article-2',
-    url: 'https://my-porfolio-common-library.vercel.app/',
-    title: 'My Porfolio Common Library',
-    description:
-      'a React UI library with a comprehensive set of components for My Porfolio page.',
-  },
-  {
-    id: 'article-3',
-    url: 'https://my-porfolio-server.herokuapp.com/graphql',
-    title: 'My Porfolio Server',
-    description:
-      'a server using Graphql which store and dispatch data for My Porfolio page.',
-  },
+	{
+		id: "article-1",
+		thumbnail: imgMediumSrc,
+		url: "https://medium.com/geekculture/how-to-manage-states-in-react-applications-effectively-d31b2f740496",
+		title: "How to manage states in React applications effectively",
+		description:
+			"you can keep not only a clean and readable state management but also reduce the complex logic handling in your applications.",
+	},
+	{
+		id: "article-2",
+		url: "https://my-portfolio-common-library.vercel.app/",
+		title: "My Porfolio Common Library",
+		description:
+			"a React UI library with a comprehensive set of components for My Porfolio page.",
+	},
+	{
+		id: "article-3",
+		url: "https://my-porfolio-server.herokuapp.com/graphql",
+		title: "My Porfolio Server",
+		description:
+			"a server using Graphql which store and dispatch data for My Porfolio page.",
+	},
 ];
 
 export default articles;
